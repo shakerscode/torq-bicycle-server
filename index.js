86b44ad9e33b70a220495596befa91f86a453861
Shaker Ahamed <ahamedshaker7@gmail.com>
2022-05-23 12:04:31 +0600

    successfully loaded product data

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.olmkj.mongodb.net/?retryWrites=true&w=majority`;
+
+ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() { 
+
+    try{
+        await client.connect();
+        const partsCollection = client.db("torqBicycle").collection("productParts");
+
+
+        app.get('/product', async(req, res)=>{
+            const result = await partsCollection.find({}).toArray();
+            res.send(result);
+        })
+
+    }
+    finally{
+        
+    }
+  }
+  run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World From Tork bicycle!')
+})
+
+app.listen(port, () => {
+  console.log(`TorkBicycle app listening on port ${port}`)
+})
